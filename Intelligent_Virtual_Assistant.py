cf98b80e79c2eded4711cfd09c15964db9e799eb
Tuavsn <43782459+Tuavsn@users.noreply.github.com>
2021-12-02 20:54:53 +0700

    Update Intelligent_Virtual_Assistant.py

--- Intelligent_Virtual_Assistant.py
+++ Intelligent_Virtual_Assistant.py
@@ -1,26 +1,56 @@
-import pyttsx3
-import speech_recognition
+#-----------IMPORTANT!!!! => YÊU CẦU:   -----------------#
+# MỞ TERMINAL =>    pip install pywin32
+#                   pip install pyttsx3
+#                   pip install speechrecognition
+#                   pip install pipwin
+#                   pipwin install pyaudio
+
+#---------------------------//------------------------------#
+import pyttsx3  #THƯ VIỆN NÓI
+import speech_recognition #THƯ VIỆN NHẬN DIỆN GIỌNG NÓI
+
 ear=speech_recognition.Recognizer()
-engine=pyttsx3.init()
+engine=pyttsx3.init() #TẠO OBJECT ENGINE
+voice=engine.getProperty('voices')
+engine.setProperty('voice',voice[1].id)
 while True:
     with speech_recognition.Microphone() as mic:
         print("say something :)")
         audio = ear.listen(mic)
     try:
         you = ear.recognize_google(audio)
-    except:
+    except speech_recognition.UnknownValueError:
         robot = "what"
-    if you=="hello":
-        robot="hello sir"
-    elif you=="goodbye":
+    if "goodbye" in you:
         robot="goodbye Tuan"
         engine.say(robot)
         print(robot)
         engine.runAndWait()
         break
+    elif "hello" in you:
+        print("""                                                                                       
+        @@@    @@@
+        @@@    @@@   @@@
+        @@@@@&@@@@   @@@
+        @@@    @@@   @@@                @
+        @@@    @@@   @@@       &@@@@@@@%@
+                            @@@@@@ @@@@  @ @ @
+                            %@@@@@ @@ @@  @@@@
+                            @@&@@@@@@@@@@@@@
+                            @@@@@@@@@@@@@@@@                   
+                    @    @@@ @@@@@ @@@ @@@ @   @             
+                    @@  @@@@ @@&@@ @@@ @@@ @@ @@             
+                    @@     @@ @@@&@ @@@ @@@      @@           
+                            @@ @@@@@ @@@ @@@                  
+                @@        @@ @@@@@ @@@ @@@        @@@        
+                    @@@ @@@@@@@@@@@@@@@@&@  @ @@@           
+                        @@ @@@@@@@@@@@@@@@@  @                
+                                &%       &                      
+                                @@       @                      
+                            @@@      @@@&                  
+                            @@@@@@@  @@@@@@@                   
+                """)
+        robot="hello sir"
     print(robot)
     engine.say(robot)
     engine.runAndWait()
-    '''
-    làm Game đi làm Game tốt cho sức khỏe đặc biệt là người già trẻ nhỏ
-    '''

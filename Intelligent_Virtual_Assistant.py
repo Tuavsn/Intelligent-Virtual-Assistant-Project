2a163fde29379df4d76dd616c7aa1f0d8ca9e28e
Tuavsn <43782459+Tuavsn@users.noreply.github.com>
2021-12-03 22:42:33 +0700

    rewrite project

--- Intelligent_Virtual_Assistant.py
+++ Intelligent_Virtual_Assistant.py
@@ -1,26 +1,88 @@
-import pyttsx3
-import speech_recognition
-ear=speech_recognition.Recognizer()
-engine=pyttsx3.init()
-while True:
+#-----------IMPORTANT!!!! => YÊU CẦU:   -----------------#
+# MỞ TERMINAL =>    pip install pywin32
+#                   pip install pyttsx3
+#                   pip install speechrecognition
+#                   pip install pipwin
+#                   pipwin install pyaudio
+
+#---------------------------//------------------------------#
+#-----------------IMPORT THƯ VIỆN--------------------#
+import pyttsx3                                  # THƯ VIỆN NÓI
+import speech_recognition                       # THƯ VIỆN NHẬN DIỆN GIỌNG NÓI
+from datetime import date, datetime             # THƯ VIỆN NGÀY THÁNG
+#----------------ASSISTANT MACHINE-------------------#
+# KHỞI TẠO
+ai_brain = ' '
+ai_ear = speech_recognition.Recognizer()        
+ai_mouth = pyttsx3.init()              
+voice=ai_mouth.getProperty('voices')
+ai_mouth.setProperty('voice',voice[1].id)       # Thiết lập giọng nữ cho AI ( voice[0]: giọng nam )
+#------------------ASSISTANT METHOD------------------#
+def start_ai():
     with speech_recognition.Microphone() as mic:
-        print("say something :)")
-        audio = ear.listen(mic)
+        print("Hello sir :\"> ")                    
+        print("How can I help you?")                
+        ai_ear.adjust_for_ambient_noise(mic)        # Giảm tiếng ồn cho microphone
+        audio = ai_ear.record(mic, duration=3)      # Ai lắng nghe giọng nói của bạn qua microphone rồi lưu vào biến audio
+        print("\nAi: ...")
     try:
-        you = ear.recognize_google(audio)
+        your_input = ai_ear.recognize_google(audio, language='vi-VN')    #Ai nhận diện giọng nói của bạn thông qua biến audio
     except:
-        robot = "what"
-    if you=="hello":
-        robot="hello sir"
-    elif you=="goodbye":
-        robot="goodbye Tuan"
-        engine.say(robot)
-        print(robot)
-        engine.runAndWait()
-        break
-    print(robot)
-    engine.say(robot)
-    engine.runAndWait()
-    '''
-    làm Game đi làm Game tốt cho sức khỏe đặc biệt là người già trẻ nhỏ
-    '''
+        print('Oops!, Something wrong :"<, you can type your input here:\n')
+        your_input=str(input("Your input: "))
+    return your_input
+#------------------MAIN()-----------------------------#
+if __name__ == '__main__':
+    start_button=str(input())
+    if start_button=="start":
+        start_ai()
+    
+
+
+
+
+
+# engine=pyttsx3.init()                           #TẠO OBJECT ENGINE
+# voice=engine.getProperty('voices')
+# engine.setProperty('voice',voice[1].id)
+# while True:
+#     with speech_recognition.Microphone() as mic:
+#         print("say something :)")
+#         audio = ear.listen(mic)
+#     try:
+#         you = ear.recognize_google(audio)
+#     except speech_recognition.UnknownValueError:
+#         robot = "what"
+#     if "goodbye" in you:
+#         robot="goodbye Tuan"
+#         engine.say(robot)
+#         print(robot)
+#         engine.runAndWait()
+#         break
+#     elif "hello" in you:
+#         print("""                                                                                       
+#         @@@    @@@
+#         @@@    @@@   @@@
+#         @@@@@&@@@@   @@@
+#         @@@    @@@   @@@                @
+#         @@@    @@@   @@@       &@@@@@@@%@
+#                             @@@@@@ @@@@  @ @ @
+#                             %@@@@@ @@ @@  @@@@
+#                             @@&@@@@@@@@@@@@@
+#                             @@@@@@@@@@@@@@@@                   
+#                     @    @@@ @@@@@ @@@ @@@ @   @             
+#                     @@  @@@@ @@&@@ @@@ @@@ @@ @@             
+#                     @@     @@ @@@&@ @@@ @@@      @@           
+#                             @@ @@@@@ @@@ @@@                  
+#                 @@        @@ @@@@@ @@@ @@@        @@@        
+#                     @@@ @@@@@@@@@@@@@@@@&@  @ @@@           
+#                         @@ @@@@@@@@@@@@@@@@  @                
+#                                 &%       &                      
+#                                 @@       @                      
+#                             @@@      @@@&                  
+#                             @@@@@@@  @@@@@@@                   
+#                 """)
+#         robot="hello sir"
+#     print(robot)
+#     engine.say(robot)
+#     engine.runAndWait()

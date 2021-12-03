30aaad0054b4b2bfe4cf1b80bc9247fdbb24499d
Tuavsn <43782459+Tuavsn@users.noreply.github.com>
2021-12-03 22:57:05 +0700

    Rewrite Assistant machine :3

--- Intelligent_Virtual_Assistant.py
+++ Intelligent_Virtual_Assistant.py
@@ -6,51 +6,33 @@
 #                   pipwin install pyaudio
 
 #---------------------------//------------------------------#
-import pyttsx3  #THƯ VIỆN NÓI
-import speech_recognition #THƯ VIỆN NHẬN DIỆN GIỌNG NÓI
-
-ear=speech_recognition.Recognizer()
-engine=pyttsx3.init() #TẠO OBJECT ENGINE
-voice=engine.getProperty('voices')
-engine.setProperty('voice',voice[1].id)
-while True:
+#-----------------IMPORT THƯ VIỆN--------------------#
+import pyttsx3                                  # THƯ VIỆN NÓI
+import speech_recognition                       # THƯ VIỆN NHẬN DIỆN GIỌNG NÓI
+from datetime import date, datetime             # THƯ VIỆN NGÀY THÁNG
+#----------------ASSISTANT MACHINE-------------------#
+# KHỞI TẠO
+ai_brain = ' '
+ai_ear = speech_recognition.Recognizer()        
+ai_mouth = pyttsx3.init()              
+voice=ai_mouth.getProperty('voices')
+ai_mouth.setProperty('voice',voice[1].id)       # Thiết lập giọng nữ cho AI ( voice[0]: giọng nam )
+#------------------ASSISTANT METHOD------------------#
+def start_ai():
     with speech_recognition.Microphone() as mic:
-        print("say something :)")
-        audio = ear.listen(mic)
+        print("Hello sir :\"> ")                    
+        print("How can I help you?")                
+        ai_ear.adjust_for_ambient_noise(mic)        # Giảm tiếng ồn cho microphone
+        audio = ai_ear.record(mic, duration=3)      # Ai lắng nghe giọng nói của bạn qua microphone rồi lưu vào biến audio
+        print("\nAi: ...")
     try:
-        you = ear.recognize_google(audio)
-    except speech_recognition.UnknownValueError:
-        robot = "what"
-    if "goodbye" in you:
-        robot="goodbye Tuan"
-        engine.say(robot)
-        print(robot)
-        engine.runAndWait()
-        break
-    elif "hello" in you:
-        print("""                                                                                       
-        @@@    @@@
-        @@@    @@@   @@@
-        @@@@@&@@@@   @@@
-        @@@    @@@   @@@                @
-        @@@    @@@   @@@       &@@@@@@@%@
-                            @@@@@@ @@@@  @ @ @
-                            %@@@@@ @@ @@  @@@@
-                            @@&@@@@@@@@@@@@@
-                            @@@@@@@@@@@@@@@@                   
-                    @    @@@ @@@@@ @@@ @@@ @   @             
-                    @@  @@@@ @@&@@ @@@ @@@ @@ @@             
-                    @@     @@ @@@&@ @@@ @@@      @@           
-                            @@ @@@@@ @@@ @@@                  
-                @@        @@ @@@@@ @@@ @@@        @@@        
-                    @@@ @@@@@@@@@@@@@@@@&@  @ @@@           
-                        @@ @@@@@@@@@@@@@@@@  @                
-                                &%       &                      
-                                @@       @                      
-                            @@@      @@@&                  
-                            @@@@@@@  @@@@@@@                   
-                """)
-        robot="hello sir"
-    print(robot)
-    engine.say(robot)
-    engine.runAndWait()
+        your_input = ai_ear.recognize_google(audio, language='vi-VN')    #Ai nhận diện giọng nói của bạn thông qua biến audio
+    except:
+        print('Oops!, Something wrong :"<, you can type your input here:\n')
+        your_input=str(input("Your input: "))
+    return your_input
+#------------------MAIN()-----------------------------#
+if __name__ == '__main__':
+    start_button=str(input())
+    if start_button=="start":
+        start_ai()
